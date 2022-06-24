4035327f1d567ff3cd529065b8925dbeec91d08d
Dhruv <ds4365778@gmail.com>
2022-06-24 14:08:57 +0530

    Evaluation Test

--- ev1.js
+++ ev1.js
@@ -1,187 +1,190 @@
 $(document).ready(function () {
-    // event.defaultPrevented();
-    $('#form').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 3,
-                onlytext: true,
-                 },
-            age:{
-                required: true,
-                min: 18,
-                max: 25,
-                age:true,
-            },
-            phone: {
-                required: true,
-                minlength: 10,
-                maxlength: 10,
-                phone: true,
-                
-            },
-            email: {
-                required: true,
-                minlength:5,
-               email:true,
-                
-            },
-            gender: {
-                required: true,
-            },
-            sunday: {
-                required: true,
-            },
-            monday: {
-                required: true,
-            },
-            tuesday: {
-                required: true,
-            },
-            wednesday: {
-                required: true,
-            },
-            thursday: {
-                required: true,
-            },
-            friday: {
-                required: true,
-            }, 
-            saturday: {
-                required: true,
-            },
-            sun: {
-                required: true,
-                min: 0,
-                max: 23
-              },
-              mon: {
-                required: true,
-                min: 0,
-                max: 23
-              },
-              tues: {
-                required: true,
-                min: 0,
-                max: 23
-              },
-              wed: {
-                required: true,
-                min: 0,
-                max: 23
-              },
-              thurs: {
-                required: true,
-                min: 0,
-                max: 23
-              },
-              fri: {
-                required: true,
-                min: 0,
-                max: 23
-              },
-              sat: {
-                required: true,
-                min: 0,
-                max: 23
-              },
-            
-            config : {
-                required:true,
-            },
+  $('#form').validate(
+    {
+      rules: {
+        name: {
+          required: true,
+          minlength: 3,
+          maxlength:15,
+          onlytext: true,
+        },
+        
+        phone: {
+          required: true,
+          minlength: 10,
+          maxlength: 10,
+          phone: true,
 
         },
-        errorPlacement: function (error, element) {
-            if (element.attr("name") == "config"){
-               error.insertAfter('#here')
-           }
-           else{
-             error.insertAfter(element);
-           }
-       },
-    
-        messages : {
-            name: {
-                required: "Enter Name",
-                minlength: "Name should be atleast 3 Characters",
-                onlytext:"Name should be only text.",
-            },
-            age: {
-                required:"Enter an age",
-                min:"minimum required age is 18.",
-                max:"maximum required age is 25.",
-            },
-            phone:
-            {
-                required: "Enter Phone number",
-                minlength: "phone number should be 10 characters",
-                maxlength: "phone number should be 10 characters",
-                
-            },
-            email: {
-                required: "The email should be in the format : abc@tntra.io",
-            },
-            
-            gender: {
-                required: "Please select a gender",
-            },
-            sun: {
-                required: "Please select time.",
-              },
-              mon: {
-                required: "Please select time.",
-              },
-              tues: {
-                required: "Please select time.",
-              },
-              wed: {
-                required: "Please select time.",
-              },
-              thurs: {
-                required: "Please select time.",
-              },
-              fri: {
-                required: "Please select time.",
-              },
-              sat: {
-                required: "Please select time.",
-              },
-            config: {
-                required: "Please select checkbox",
-            },
-            sunday: {
-                required: "Please select the checkbox",
-            },
-            monday: {
-                required: "Please select the checkbox",
-            },
-            tuesday: {
-                required: "Please select the checkbox",
-            },
-            wednesday: {
-                required: "Please select the checkbox",
-            },
-            thursday: {
-                required: "Please select the checkbox",
-            },
-            friday: {
-                required: "Please select the checkbox",
-            },
-            saturday: {
-                required: "Please select the checkbox",
-            },
+        email: {
+          required: true,
+          minlength: 5,
+          email: true,
+
         },
-        // errorPlacement: function (error, element) {
-        //   if (element.is(":radio")) {
-        //     error.appendTo('#radio');
-        //   }
-        //   else {
-        //     error.insertAfter(element);
-        //   }
+        gender: {
+          required: true
+        },
+
+
+        age: {
+          required: true,
+          min: 18,
+          max: 25,
+          age: true,
+        },
+        config:{
+          required:true,
+        },
+        sun: {
+          required: true,
+          min: 0,
+          max: 23
+        },
+        mon: {
+          required: true,
+          min: 0,
+          max: 23
+        },
+        tues: {
+          required: true,
+          min: 0,
+          max: 23
+        },
+        wed: {
+          required: true,
+          min: 0,
+          max: 23
+        },
+        thurs: {
+          required: true,
+          min: 0,
+          max: 23
+        },
+        fri: {
+          required: true,
+          min: 0,
+          max: 23
+        },
+        sat: {
+          required: true,
+          min: 0,
+          max: 23
+        },
+        // sunday: {
+        //   required: true,
         // },
-        
-      }); 
+        // monday: {
+        //   required:true,
+        // },
+        // tuesday: {
+        //   required:true,
+        // },
+        // wednesday: {
+        //   required:true,
+        // },
+        // thursday: {
+        //   required:true,
+        // },
+        // friday: {
+        //   required:true,
+        // },
+        // saturday: {
+        //   required:true,
+        // },
+      },
+      errorPlacement: function (error, element) {
+         if (element.attr("name") == "config"){
+            error.insertAfter('#here')
+        }
+        else{
+          error.insertAfter(element);
+        }
+    },
+
+
+
+      messages: {
+        name: {
+          required: "Enter Your Name",
+          minlength: "Name should be atleast 3 Characters",
+          maxlength: "maximum length of name is 15 characters",
+          onlytext: "Name should be only text."
+        },
+        age: {
+          required: "Enter Your age",
+          min: "minimum required age is 18.",
+          max: "maximum required age is 25.",
+        },
+        phone:
+        {
+          required: "Enter  Your Phone number",
+          minlength: "Phone number should be 10 characters",
+          maxlength: "Phone number should be 10 characters",
+
+        },
+        email: {
+          required: "The email should be in the format :abc@tntra.io",
+        },
+
+        gender: {
+          required: "Please select a gender",
+        },
+        config:{
+          required:"Please  select the checkbox "
+        },
+        sun: {
+          required: "Please select time."
+        },
+        mon: {
+          required: "Please select time."
+        },
+        tues: {
+          required: "Please select time."
+        },
+        wed: {
+          required: "Please select time."
+        },
+        thurs: {
+          required: "Please select time."
+        },
+        fri: {
+          required: "Please select time."
+        },
+        sat: {
+          required: "Please select time."
+        },
+        // sunday: {
+        //   required: "Please select checkbox"
+        // },
+        // monday: {
+        //   required: "Please select checkbox"
+        // },
+        // tuesday: {
+        //   required: "Please select checkbox"
+        // },
+        // wednesday: {
+        //   required: "Please select checkbox"
+        // },
+        // thursday: {
+        //   required: "Please select checkbox"
+        // },
+        // friday: {
+        //   required: "Please select checkbox"
+        // },
+        // saturday: {
+        //   required: "Please select checkbox"
+        // },
+
+      }
     });
+  $('input:checkbox').on('change', function () {
+    if (!$(this).is(':checked')) $(this).closest('#sunday').find('select').val('').change()
+  })
+
+}); 
+      
+   
     $('config').on('change', function () {
         if (!$(this).is(':checked')) $(this).closest('#sunday').find('select').val('');
     
@@ -204,72 +207,65 @@ $(document).ready(function () {
     );
     
     $.validator.addMethod(
-      "phone",
-      function (value) {
-        return /[0-9]$/.test(value);
-      },
-      "enter valid phone number"
-    );
+        "phone",
+        function (value) {
+          return /[7-9]{1}[0-9]{9}$/.test(value);
+        },
+        "enter valid phone number"
+      );
     $.validator.addMethod(
       "email",
       function (value) {
         return /^([A-Za-z0-9_\-\.])+\@([tntra|gmail])+\.(io)$/ .test(value);
       },
-      "enter a valid mail"
+      "The email should be in the format :abc@tntra.io"
     );
 
-    $(function() {
-      enable_cb();   
-      $("#config").click(enable_cb);
-      disable_cb();
-      $('#config').click(disable_cb);
-    });
-    
-  function enable_cb() {
+    $('#config').click(cb_func);
+     cb_func();
+
+  
+  
+  function cb_func() {
     if (!this.checked) {
-        document.getElementById("sunday").checked=false
-        document.getElementById("monday").checked=false
-        document.getElementById("tuesday").checked=false
-        document.getElementById("wednesday").checked=false
-        document.getElementById("thursday").checked=false
-        document.getElementById("friday").checked=false
-        document.getElementById("saturday").checked=false
-        $("#sun")[0].value="None";
+      document.getElementById("sunday").checked=false;
+      document.getElementById("monday").checked=false;
+      document.getElementById("tuesday").checked=false;
+      document.getElementById("wednesday").checked=false;
+      document.getElementById("thursday").checked=false;
+      document.getElementById("friday").checked=false;
+      document.getElementById("saturday").checked=false;
+      $("#sun")[0].value="None";
       $("#mon")[0].value="None";
       $("#tues")[0].value="None";
       $("#wed")[0].value="None";
       $("#thurs")[0].value="None";
       $("#fri")[0].value="None";
       $("#sat")[0].value="None";
+            
+      $("#sunday").attr('disabled', true);
+      $("#monday").attr('disabled', true);
+      $("#tuesday").attr('disabled', true);
+      $("#wednesday").attr('disabled', true);
+      $("#thursday").attr('disabled', true);
+      $("#friday").attr('disabled', true);
+      $("#saturday").attr('disabled', true);
+      $('#sun').attr('disabled', true);
+      $('#mon').attr('disabled', true);
+      $('#tues').attr('disabled', true);
+      $('#wed').attr('disabled', true);
+      $('#thurs').attr('disabled', true);
+      $('#fri').attr('disabled', true);
+      $('#sat').attr('disabled', true);
+    }else{
+      $("#sunday").removeAttr("disabled");
+      $("#monday").removeAttr("disabled");
+      $("#tuesday").removeAttr("disabled");
+      $("#wednesday").removeAttr("disabled");
+      $("#thursday").removeAttr("disabled");
+      $("#friday").removeAttr("disabled");
+      $("#saturday").removeAttr("disabled");
     }
-      if (this.checked) {
-          $("#sunday").removeAttr("disabled");
-          $("#monday").removeAttr("disabled");
-          $("#tuesday").removeAttr("disabled");
-          $("#wednesday").removeAttr("disabled");
-          $("#thursday").removeAttr("disabled");
-          $("#friday").removeAttr("disabled");
-          $("#saturday").removeAttr("disabled");
-      } 
-  }
-  function disable_cb(){
-      if (!this.checked){
-          $("#sunday").attr('disabled',true);
-          $("#monday").attr('disabled',true);
-          $("#tuesday").attr('disabled',true);
-          $("#wednesday").attr('disabled',true);
-          $("#thursday").attr('disabled',true);
-          $("#friday").attr('disabled',true);
-          $("#saturday").attr('disabled',true);
-          $('#sun').attr('disabled',true);
-            $('#mon').attr('disabled',true);
-            $('#tues').attr('disabled',true);
-            $('#wed').attr('disabled',true);
-            $('#thurs').attr('disabled',true);
-            $('#fri').attr('disabled',true);
-            $('#sat').attr('disabled',true);
-          
-      }
   }
 
 
@@ -444,22 +440,23 @@ function disable_cb7(){
             $('#email').valid() &&
             
             $('#phone').valid() &&
-            $('#sun').valid() &&
-            $('#mon').valid() &&
-            $('#tues').valid() &&
-            $('#wed').valid() &&
-            $('#thurs').valid() &&
-            $('#fri').valid() &&
-            $('#sat').valid() &&
-            $('#monday').valid() &&
-            $('#tuesday').valid() &&
-            $('#wednesday').valid() &&
-            $('#thursday').valid() &&
-            $('#friday').valid() &&
-            $('#saturday').valid() &&
+            
             
            
-            $("input[name='gender']:checked").val() != undefined
+            $("input[name='gender']:checked").val() != undefined &&
+            $('#sun').valid() ||
+            $('#mon').valid() ||
+            $('#tues').valid() ||
+            $('#wed').valid() ||
+            $('#thurs').valid() ||
+            $('#fri').valid() ||
+            $('#sat').valid() 
+            // $('#monday').valid() &&
+            // $('#tuesday').valid() &&
+            // $('#wednesday').valid() &&
+            // $('#thursday').valid() &&
+            // $('#friday').valid() &&
+            // $('#saturday').valid() 
             )
             {
 
@@ -488,3 +485,6 @@ function disable_cb7(){
             
 }
 )
+
+
+
--- table.js
+++ table.js
@@ -2,16 +2,14 @@ let name = localStorage.getItem('name');
 let age = localStorage.getItem('age');
 let email = localStorage.getItem('email');
 let phone = localStorage.getItem('phone');
-
-
 let gender = localStorage.getItem('gender');
-let sunday=  localStorage.getItem('sunday',$('#sun').val());
- let monday =               localStorage.getItem('monday',$('#mon').val());  
-   let tuesday =            localStorage.getItem('tuesday',$('#tues').val());
-    let wednesday =            localStorage.getItem('wednesday',$('#wed').val());  
-     let thursday=           localStorage.getItem('thursday',$('#thurs').val());   
-      let friday=          localStorage.getItem('friday',$('#fri').val());
-     let saturday= localStorage.getItem('saturday',$('#sat').val());
+let sunday = localStorage.getItem('sunday', $('#sun').val())=="None"?"":localStorage.getItem('sunday', $('#sun').val());
+let monday = localStorage.getItem('monday', $('#mon').val())=="None"?"":localStorage.getItem('monday', $('#mon').val());
+let tuesday =localStorage.getItem('tuesday', $('#tue').val())=="None"?"":localStorage.getItem('tuesday', $('#tue').val());
+let wednesday = localStorage.getItem('wednesday', $('#wed').val())=="None"?"":localStorage.getItem('wednesday', $('#wed').val());
+let thursday = localStorage.getItem('thursday', $('#thurs').val())=="None"?"":localStorage.getItem('thursday', $('#thurs').val());
+let friday = localStorage.getItem('friday', $('#fri').val())=="None"?"":localStorage.getItem('friday', $('#fri').val());
+let saturday = localStorage.getItem('saturday', $('#sat').val())=="None"?"":localStorage.getItem('saturday', $('#sat').val());
 
 
 
